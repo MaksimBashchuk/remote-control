ef1246b6deebf1efc1817ed6dc3d09a27b8c45ac
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2022-06-26 02:26:37 +0300

    feat: add logging utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,19 @@
 export const parseArguments = (str: string) => {
   return str.split(' ');
 };
+
+export const logReceivedCommand = (msg: string) => {
+  console.log(`Received command: ${msg}\n`);
+};
+
+export const logSuccess = (command: string) => {
+  console.log(`\x1b[32mCommand ${command} was executed successfully\x1b[0m`);
+};
+
+export const logError = (command: string) => {
+  console.error(`\x1b[31mCommand ${command} was failed\x1b[0m`);
+};
+
+export const logInfo = (msg: string) => {
+  console.log(`\x1b[35m${msg}\x1b[0m`);
+};
